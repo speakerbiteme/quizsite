b16b3eee01c44649ab8bdf0616e82ea03fb76b73
Elisabeth S <amagerdigte@hotmail.com>
2015-08-06 13:33:37 +0200

    Ändrade namn på Fråga och Slut och lade till Views

--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+def startpage(request):
+	return render(request, "quiz/start.html")
+def quiz(request):
+	return render(request, "quiz/quizstart.html")
+def question(request):
+	return render(request, "quiz/question.html")
+def completed(request):
+	return render(request, "quiz/completed.html")
+
 # Create your views here.
