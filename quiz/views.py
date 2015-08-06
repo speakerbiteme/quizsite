662efcc99e8dda4431eca9a3201e6ab005d01377
Elisabeth S <amagerdigte@hotmail.com>
2015-08-06 15:19:16 +0200

    Lagt till lite data på quiz- + frågesidan

--- quiz/views.py
+++ quiz/views.py
@@ -19,10 +19,25 @@ def startpage(request):
 		"quizzes": quizzes,
 	}
 	return render(request, "quiz/startpage.html", context)
-def quiz(request):
-	return render(request, "quiz/quiz.html")
-def question(request):
-	return render(request, "quiz/question.html")
+
+def quiz(request, slug):
+	context = {
+		"quiz": quizzes[slug],
+		"quiz_slug": slug,
+	}
+	return render(request, "quiz/quiz.html", context)
+
+def question(request, slug, number):
+	context = {
+		"question_number": number,
+	    "question": u"Hur många bultar har ölandsbron?",
+		"answer1": u"12",
+	   	"answer2": u"66 400",
+	    "answer3": u"7 428 954",
+	    "quiz_slug": slug,
+	}
+	return render(request, "quiz/question.html", context)
+
 def completed(request):
 	return render(request, "quiz/completed.html")
 
--- quizsite/urls.py
+++ quizsite/urls.py
@@ -3,9 +3,10 @@ from quiz import views
 
 
 urlpatterns = [
+
 	url("^$", views.startpage),
-	url(r"^quiz/[a-z-]+/$", views.quiz),
-	url(r"^quiz/[a-z-]+/question/[0-9]/$", views.question),
+	url(r"^quiz/([a-z-]+)/$", views.quiz),
+	url(r"^quiz/([a-z-]+)/question/([0-9]+)/$", views.question),
 	url(r"^quiz/[a-z-]+/completed/$", views.completed),
 ]
 
